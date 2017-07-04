cb10850bab00ea5be9eadaa1b57737c73ea1e589
Manyanda Chitimbo <manyanda.chitimbo@gmail.com>
2017-07-04 22:14:57 +0200

    feat:

--- src/main/java/com/machi/wajinga/ws/resources/HomeResource.java
+++ src/main/java/com/machi/wajinga/ws/resources/HomeResource.java
@@ -1,16 +1,23 @@
 package com.machi.wajinga.ws.resources;
 
+import java.util.UUID;
+
+import javax.annotation.security.PermitAll;
 import javax.inject.Inject;
 import javax.ws.rs.BadRequestException;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
+
+import org.joda.time.DateTime;
 
 import com.machi.wajinga.dao.WajingaDao;
 import com.machi.wajinga.dao.mjinga.Mjinga;
@@ -40,14 +47,14 @@ public class HomeResource {
      */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Mjinga login() {
+    public Mjinga ingia() {
     		return mjingaDao.tafutaMjingaKwaJina(context.getUserPrincipal().getName());
     }
     
     @POST
     @Path("badili-nywira")
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response changePassword(NywiraKontena kontena) {
+    public Response badiliNywira(NywiraKontena kontena) {
     		Mjinga mjinga = mjingaDao.tafutaMjingaKwaJina(context.getUserPrincipal().getName());
     		
     		if (!mjinga.getBaruaPepe().equals(kontena.baruaPepe)) {
@@ -64,6 +71,36 @@ public class HomeResource {
     		
     		mjingaDao.badiliNywira(mjinga, kontena.nywiraMpya);
     		
+    		return Response.noContent().build();
+    }
+    
+    
+    @GET
+    @PermitAll
+    @Path("tengeneza-nywira-mpya/{barua-pepe}")
+    public Response tengenezaNywiraMpya(@PathParam("barua-pepe") String baruaPepe) {
+    		if (baruaPepe == null) {
+    			throw new BadRequestException("Barua pepe ni muhimu");
+    		}
+    		
+    		Mjinga mjinga = mjingaDao.tafutaMjingaKwaBaruaPepe(baruaPepe);
+    		
+    		if (mjinga == null) {
+    			throw new NotFoundException("Barua pepe haipo");
+    		}
+    		
+    		
+    		/**
+    		 * TODO
+    		 */
+    		String tokeni = UUID.randomUUID().toString();
+    		
+    		mjinga.setNywiraTokeni(tokeni);
+    		mjinga.setTrhOmbiLaKubadiliNywira(DateTime.now());
+    		
+    		/**
+    		 * Tuma barua pepe kwa mtumiaji.
+    		 */
     		return Response.noContent().build();
     }
     
